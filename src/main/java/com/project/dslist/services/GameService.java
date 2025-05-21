2f257a31fd75a236e7bbe0b19de08304d073dca2
Yohannah Véras <yohannahlv@gmail.com>
2025-05-21 20:37:53 -0300

    projection

--- src/main/java/com/project/dslist/services/GameService.java
+++ src/main/java/com/project/dslist/services/GameService.java
@@ -2,6 +2,7 @@ package com.project.dslist.services;
 
 import com.project.dslist.dto.GameDTO;
 import com.project.dslist.dto.GameMinDTO;
+import com.project.dslist.projections.GameMinProjection;
 import com.project.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -27,6 +28,12 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
